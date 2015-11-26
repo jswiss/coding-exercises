52e86989059f5923148be2c1cc41a8501ec29370
Joshua Swiss <swissjoshua@gmail.com>
2015-11-26 14:07:49 +0300

    commented chessboard.js

--- js/chessboard.js
+++ js/chessboard.js
@@ -1,14 +1,19 @@
+// sets board size, width and height
 var size = 8;
 
+// creates a blank board
 var board = "";
 
+//sets x and y to be less than 8 chars in length
 for (var x = 0; x < size; x++) {
     for (var y = 0; y < size; y++) {
+        //alternates between hash and space on board
         if ((x + y) % 2 === 0) 
             board += " ";
          else 
             board += "#";
     }
+    //adds a line break for each row
     board += "\n";
 }
 
