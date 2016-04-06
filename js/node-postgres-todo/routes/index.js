afa9b65f8eeaec79a7b8325bcb58ece1197bc739
Joshua Swiss <swissjoshua@gmail.com>
2016-04-06 17:39:53 +0300

    added GET endpoint

--- js/node-postgres-todo/routes/index.js
+++ js/node-postgres-todo/routes/index.js
@@ -47,4 +47,36 @@ router.post('/api/v1/todos', function(req, res) {
   });
 });
 
+router.get('/api/v1/todos', function(req, res) {
+
+  var results = [];
+
+  //Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+
+    //Handle connection errors
+    if(err) {
+      done();
+      console.log('Get connection error ', err);
+      return res.status(500).json({ success: false, data: err });
+    }
+
+    //SQL Query ==> Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+    //Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    //After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+
+
 module.exports = router;
