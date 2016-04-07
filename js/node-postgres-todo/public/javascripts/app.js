c8659983e3647a164faaeff475525d50be891a1c
Joshua Swiss <swissjoshua@gmail.com>
2016-04-07 12:15:54 +0300

    added frontend post and delete requests

--- js/node-postgres-todo/public/javascripts/app.js
+++ js/node-postgres-todo/public/javascripts/app.js
@@ -6,12 +6,31 @@ angular.module('nodeTodo', [])
     $scope.todoData = {};
 
     //Get all todos
-    $http.get('/apiv1/todos')
+    $http.get('/api/v1/todos')
       .success(function(data) {
         $scope.todoData = data;
-        console.log("Data: ", data);
+        console.log("Get data: ", data);
       })
       .error(function(error) {
         console.log("Error: ", error);
       });
+
+    $http.post('/api/v1/todos', $scope.formData)
+      .success(function(data) {
+        $scope.formData = {};
+        $scope.todoData = data;
+        console.log("Post data: ", data);
+      })
+      .error(function(error) {
+        console.log("Post error: ", error);
+      });
+
+      $http.delete('/api/v1/todos' + todoID)
+        .success(function(data) {
+          $scope.todoData = data;
+          console.log("Delete data: ", data);
+        })
+        .error(function(error) {
+          console.log("Delete error: ", error)
+        });
   });
